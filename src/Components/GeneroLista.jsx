e0ec43f5daab6d16b0032f4b31e0efcf9a6cd390
Alén <alenm.1717@gmail.com>
2023-08-30 20:19:56 -0300

    yendo

--- src/Components/GeneroLista.jsx
+++ src/Components/GeneroLista.jsx
@@ -1,19 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import GlobalApi from '../Services/GlobalApi'
 
 
 const GeneroLista = () => {
 
+    const [generoLista, setGeneroLista] = useState([])
     useEffect(() => {
         getGeneroLista();
     }, [])
     const getGeneroLista= () => {
         GlobalApi.getGeneroLista.then((resp) => {
-            console.log(resp.data.results)
+            console.log(resp.data.results);
+            setGeneroLista(resp.data.results);
         })
     }
   return (
-    <div>GeneroLista ---</div>
+    <div>
+        <h2 className='text-[30px] font-bold dark:text-white'>Géneros</h2>
+        {generoLista.map((item) =>(
+            <div className='flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 p-2 group
+            rounded-lg hover:dark:bg-gray-600 '>
+        <img src={item.image_background} className='w-[40px] h-[40px] object-cover rounded-lg group-hover:scale-105 transition-all ease-out duration-300' />
+<h3 className='dark:text-white text-[18px] group-hover:font-bold '>{item.name}</h3>
+            </div>
+        ))}
+    </div>
   )
 }
 
--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -3,8 +3,8 @@ import GeneroLista from "../Components/GeneroLista"
 
 const Inicio = () => {
   return (
-    <div className='grid grid-cols-4'>
-      <div className='bg-red-600 h-full hidden md:block'>
+    <div className='grid grid-cols-4 px-8'>
+      <div className=' hidden md:block'>
         <GeneroLista />
       </div>
       <div className='col-span-4 md:col-span-3 bg-blue-400'>Lista de Juegos</div>
