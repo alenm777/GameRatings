287c153b76e9e9d9532413bd99619aae9da6d3a8
Alén <alenm.1717@gmail.com>
2023-09-02 19:46:20 -0300

    mentirosa

--- src/Components/JuegosPorGeneroId.jsx
+++ src/Components/JuegosPorGeneroId.jsx
@@ -1,11 +1,26 @@
 import React, {useEffect} from 'react'
 
-const JuegosPorGeneroId = ({gameList}) => {
+function JuegosPorGeneroId({gameList})  {
 useEffect(() => {
 console.log("Lista de Juegos", gameList )
 },[])
   return (
-    <div>JuegosPorGeneroId</div>
+    <div>
+        <h2 className='font-bold text-[30px] dark:text-white mt-5'>Juegos más Destacados</h2>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3
+    gap-6 mt-5'>
+        {gameList.map((item)=> (
+            <div className='bg-[#76a8f75e] p-3 rounded-lg pb-12 h-full
+            hover:scale-110 translate-all ease-in-out duration-300 cursor-pointer'>
+                <img src={item.background_image} className='w-full h-[80%] rounded-xl object-cover'/>
+                <h2 className='text-[20px] dark:text-white font-bold
+                '>{item.name}<span className='p-1 rounded-sm ml-2 text-[10px] bg-green-100 text-green-700 font-medium'>{item.metacritic}</span></h2>
+                <h2 className='text-gray-500 dark:text-gray-300'>
+                    ⭐{item.rating}        💬{item.reviews_count}       🔥{item.suggestions_count}</h2>
+            </div>
+        ))}
+    </div>
+    </div>
   )
 }
 
