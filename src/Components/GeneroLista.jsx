83a9aef04f2b45a11737662fa46855244d4589b6
Alén <alenm.1717@gmail.com>
2023-09-02 18:30:56 -0300

    perro primo

--- src/Components/GeneroLista.jsx
+++ src/Components/GeneroLista.jsx
@@ -11,6 +11,7 @@ const GeneroLista = () => {
     }, [])
     const getGeneroLista= () => {
         GlobalApi.getGeneroLista.then((resp) => {
+    
             setGeneroLista(resp.data.results);
         })
     }
--- src/Components/JuegosPopulares.jsx
+++ src/Components/JuegosPopulares.jsx
@@ -5,13 +5,19 @@ const JuegosPopulares = ({gameList}) => {
 console.log(gameList)
     },[])
   return (
-    <div>
-        {gameList.map((item, index) => index<4&&(
-        <div>
-            <img src={item.background_image} />
+    <div className='mt-5 hidden md:block'>
+        <h2 className='font-bold text-[30px] dark:text-white'>Juegos más populares</h2>
+    <div className='hidden md:grid md:grid-cols-3 gap-4 mt-5 lg:grid-cols-4'>
+        {gameList.map((item, index) => index<4 &&(
+        <div className='bg-[#76a8f75e] rounded-lg group
+        hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer'>
+            <img src={item.background_image}
+             className='h-[270px] rounded-lg object-cover' />
+            <h2 className='dark:text-white p-2 text-[20px] font-bold'>{item.name}</h2>
         </div>
         ))}
     </div>
+     </div>
   )
 }
 
