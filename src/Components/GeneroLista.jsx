b0cd50f2bb3683a127dbf6e4430aea6952adaf10
Alén <alenm.1717@gmail.com>
2023-09-02 16:41:21 -0300

    Eliminada carpeta game-ratings y modificado GeneroLista.jsx

--- src/Components/GeneroLista.jsx
+++ src/Components/GeneroLista.jsx
@@ -19,7 +19,7 @@ const GeneroLista = () => {
         <h2 className='text-[30px] font-bold dark:text-white'>Géneros</h2>
         {generoLista.map((item) =>(
             <div className='flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 p-2 group
-            rounded-lg hover:dark:bg-gray-600 '>
+            rounded-lg hover:dark:bg-gray-700 '>
         <img src={item.image_background} className='w-[40px] h-[40px] object-cover rounded-lg group-hover:scale-105 transition-all ease-out duration-300' />
 <h3 className='dark:text-white text-[18px] group-hover:font-bold '>{item.name}</h3>
             </div>
