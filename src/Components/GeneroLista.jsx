08a404e9623ce1bb79d4bcb75a4243be78dd8d10
Alén <alenm.1717@gmail.com>
2023-09-02 16:56:56 -0300

    vamos yendo

--- src/Components/GeneroLista.jsx
+++ src/Components/GeneroLista.jsx
@@ -5,6 +5,7 @@ import GlobalApi from '../Services/GlobalApi'
 const GeneroLista = () => {
 
     const [generoLista, setGeneroLista] = useState([])
+    const [activeIndex, setActiveIndex] = useState(0);
     useEffect(() => {
         getGeneroLista();
     }, [])
@@ -17,10 +18,14 @@ const GeneroLista = () => {
   return (
     <div>
         <h2 className='text-[30px] font-bold dark:text-white'>Géneros</h2>
-        {generoLista.map((item) =>(
-            <div className='flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 p-2 group
-            rounded-lg hover:dark:bg-gray-700 '>
-        <img src={item.image_background} className='w-[40px] h-[40px] object-cover rounded-lg group-hover:scale-105 transition-all ease-out duration-300' />
+        {generoLista.map((item, index) =>(
+            <div 
+            onClick={() => setActiveIndex(index)}
+            className={`flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 p-2
+             group rounded-lg hover:dark:bg-gray-600
+             ${activeIndex==index?"bg-gray-300 dark:bg-gray-600": null}`}>
+        <img src={item.image_background}
+         className='w-[40px] h-[40px] object-cover rounded-lg group-hover:scale-105 transition-all ease-out duration-300' />
 <h3 className='dark:text-white text-[18px] group-hover:font-bold '>{item.name}</h3>
             </div>
         ))}
